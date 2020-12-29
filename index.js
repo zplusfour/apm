e8429464e31bed5050983690a240245cf30111ab
zplusfour <zplusfour.node@outlook.com>
2020-12-29 12:56:25 +0200

    Downloads all file from package and fixed issues

--- index.js
+++ index.js
@@ -1,23 +1,42 @@
 #!/usr/bin/env node
 const program = require('commander');
 const fs = require('fs');
+const colors = require('colors');
 const fetch = require('node-fetch');
 
+const PACKAGE_URL = "https://registry.zdev1.repl.co/package";
+
 function ApmError(err, type) {
-  console.log(`${type}: ${err}`);
+  console.log(`${type.underline.red}: ${err}`);
 }
 
 async function fetchPackage(pkg, v) {
-  const p = await fetch(`https://registry.zdev1.repl.co/package/${pkg}/${v}/main.adk`).then(res => res.text()).then((body) => {
-    fs.writeFile(`./packages/${pkg}.adk`, body, (err) => {if(err)  ApmError(err.replace('Error: ', ''), 'FileCreationError')});
-    console.log("Done!");
+  const p = await fetch(`${PACKAGE_URL}/${pkg}/${v}/`).then(res => res.json()).then((body) => {
+    //fs.writeFile(`./packages/${pkg}.adk`, body, (err) => {if(err)  {ApmError(err, 'FileCreationError')}else{console.log("Done!".underline.blue);}});
+    fs.mkdir(`./packages/${pkg}/`, (err) => {
+      if(err) ApmError(err, 'ModuleCreationError');
+    });
+    body.forEach(async (mod) => {
+      const modata = await fetch(`${PACKAGE_URL}/${pkg}/${v}/${mod}`).then(res => res.text()).then((body) => {
+        fs.writeFileSync(`./packages/${pkg}/${mod}`, body);
+      })
+    });
   })
   .catch((err) => ApmError(err, 'ApmError'));
 }
 
 function init() {
   fs.mkdirSync('./packages');
-  console.log("Done!");
+  console.log("Done!".underline.green);
+}
+
+function uninstall(pkg) {
+  fs.rmdir('./packages/'+pkg, (err) => {
+    if(err) ApmError(err, 'DirectoryDeletionError');
+    else{
+      console.log('Done!'.underline.green);
+    }
+  });
 }
 
 program
@@ -33,6 +52,6 @@ program
 program
   .command("uninstall <package>")
   .description("Uninstall a package")
-  .action((package) => {fs.unlinkSync(`./packages/${package}.adk`);console.log("Done!")})
+  .action((package) => {uninstall(package)})
 
 program.parse(process.argv);
